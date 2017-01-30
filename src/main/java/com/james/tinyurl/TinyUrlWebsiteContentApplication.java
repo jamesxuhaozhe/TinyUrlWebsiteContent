d58277cb58632da307a8ee40c12f32de9191f488
Haozhe Xu <jamesxuhaozhe@gmail.com>
2017-01-29 22:38:30 -0800

    As a developer, I would like to create a tinyurl pacakge so I can just start coding

---
+++ src/main/java/com/james/tinyurl/TinyUrlWebsiteContentApplication.java
@@ -0,0 +1,12 @@
+package com.james.tinyurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyUrlWebsiteContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyUrlWebsiteContentApplication.class, args);
+	}
+}
